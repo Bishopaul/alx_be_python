e60a9c6fe050bc2485654d34cbbf9fb321cb552d
Bishopaul <monitepaul@gmail.com>
2025-11-16 07:57:56 -0800

    Control flow

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,9 +1,9 @@
-wh_cond = input("What's the weather like today? (sunny/rainy/cold): ").lower()
-if wh_cond == "sunny":
+weather = input("What's the weather like today? (sunny/rainy/cold): ").lower()
+if weather == "sunny":
     print("Wear a t-shirt and sunglasses.")
-elif wh_cond == "rainy":
+elif weather == "rainy":
     print("Don't forget your umbrella and a raincoat.")
-elif wh_cond == "cold":
+elif weather == "cold":
     print("Make sure to wear a warm coat and a scarf.")
 else:
     print("Sorry, I don't have recommendations for this weather.")
